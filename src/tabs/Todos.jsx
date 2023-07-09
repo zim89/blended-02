0240895111e6bf270fa31645cea21eedc716df73
KalchenkoNatalya <janatka2222@gmail.com>
2023-07-09 17:05:58 +0300

    task2

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,43 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  };
+
+  handleInput = value => {
+    const newTodos = { text: value, id: nanoid() };
+    this.setState(prevState => ({ todos: [...prevState.todos, newTodos] }));
+  };
+
+  deleteTodo = id => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== id),
+    }));
+  };
+  componentDidMount() {
+    const savedtodos = JSON.parse(localStorage.getItem('todos'));
+    if (savedtodos) {
+      this.setState({ todos: savedtodos });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const {todos} = this.state;
+    if (prevState.todos !== todos) {localStorage.setItem('todos', JSON.stringify(todos));}
+  }
   render() {
-    return <Text>Todos</Text>;
+    console.log(this.state.todos);
+    return (
+      <>
+        <SearchForm onSubmit={this.handleInput} />;
+        <Grid>
+          {this.state.todos.map((todo, idx) => (
+            <GridItem key={todo.id}>
+              <Todo todo={todo} idx={idx} deleteTodo={this.deleteTodo} />
+            </GridItem>
+          ))}
+        </Grid>
+      </>
+    );
   }
 }
